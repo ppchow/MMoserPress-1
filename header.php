3796e096db15e72e278337eb9f975872b411937c
KenChow <kenC@mmoser.com>
2015-10-20 11:39:34 +0800

    Remove social link in the header

--- header.php
+++ header.php
@@ -86,10 +86,10 @@
 		        </div>
 		        <div class="medium-6 medium-pull-6 columns">
 		        	<ul class="followlink">
-		        		<li class="socialink">	
+		        		<!-- <li class="socialink">	
 		          			<?php dynamic_sidebar("header-sidebar-2");?> 
 		          		</li>
-		          		<!-- <li class="languagelink">
+		          		 <li class="languagelink">
 		          			<?php
 		          				dynamic_sidebar("header-sidebar-1");
 							 ?>
